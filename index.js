2272d56729a1bfb7805c48dcc6c8e378c95fec9b
Nirmal Kumar Singh <nirmaljobworks@gmail.com>
2025-08-21 20:42:33 +0530

    explored http methods

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("Hello today world");
-});
+app.get("/user",(req,res)=>{
+    res.send({firstNae: "Nirmal", LastName : "Singh"});
+})
+
+app.post("/user",(req,res)=>{
+    console.log("Save data in the database..");
+    res.send("Data has been successfully save in the Database...")
+})
 
 app.listen(PORT = 3000,() =>{
     console.log(`Listiening to port ${PORT}`);
